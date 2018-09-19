74f5ebc2dca92fd696ecd105d8efe5ac0d89c06d
Luke Patton <lpatton@dosomething.org>
2018-09-19 12:11:02 -0400

    Basic searchbar and css

--- src/components/Searchbar/SearchBar.js
+++ src/components/Searchbar/SearchBar.js
@@ -5,7 +5,17 @@ class SearchBar extends React.Component {
   render() {
     return (
       <div className="searchbar">
-        <p>Searchbar</p>
+        <div className="searchbar field">
+          {/* onChange function sets a state change of the term to search for */}
+          <input
+            placeholder="Search for a song, artist or album"
+            onChange="Function goes here"
+          />
+          <div className="searchbar button">
+            {/* When the button is clicked, search Spotify for the terms in state */}
+            <a onClick="Button here">Search</a>
+          </div>
+        </div>
       </div>
     );
   }
