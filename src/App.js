5930517e1c97f8c5ba15b35fece9c4e35d72f347
Luke Patton <lpatton@dosomething.org>
2018-09-20 10:54:44 -0400

    Adds the dummy method for searching Spotify
    Passes a simple state term back to the parent and executes on it.

--- src/App.js
+++ src/App.js
@@ -6,10 +6,23 @@ import List from './components/List/List';
 import PlaylistList from './components/PlaylistList/PlaylistList';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.searchSpotify = this.searchSpotify.bind(this);
+  }
+
+  searchSpotify(term) {
+    // References the Spotify utility to make a request
+    // using the terms fed by the child object that gets passed this prop
+    console.log('Searching for ' + term);
+
+    // Then sets the result state to pass to the List
+  }
   render() {
     return (
       <div className="App">
-        <Searchbar />
+        <Searchbar searchSpotify={this.searchSpotify} />
         <div className="search-results">
           <div className="column">
             <h2>Results</h2>
--- src/components/Searchbar/SearchBar.js
+++ src/components/Searchbar/SearchBar.js
@@ -2,6 +2,30 @@ import React from 'react';
 import './SearchBar.css';
 
 class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+
+    // This is what we'll pass to Spotify API
+    // It needs to change when someone types something new in our field
+    this.state = {
+      term: '',
+    };
+
+    this.handleTermChange = this.handleTermChange.bind(this);
+    this.handleSearch = this.handleSearch.bind(this);
+  }
+
+  handleTermChange(event) {
+    // When someone types (an event), change the state to that value.
+    this.setState({ term: event.target.value });
+  }
+
+  handleSearch(event) {
+    // This runs a function passed by the parent to alter it's state
+    this.props.searchSpotify(this.state.term);
+    event.preventDefault();
+  }
+
   render() {
     return (
       <div className="searchbar">
@@ -9,12 +33,12 @@ class SearchBar extends React.Component {
           {/* onChange function sets a state change of the term to search for */}
           <input
             placeholder="Search for a song, artist or album"
-            onChange="Function goes here"
+            onChange={this.handleTermChange}
           />
-          <div className="searchbar button">
-            {/* When the button is clicked, search Spotify for the terms in state */}
-            <a onClick="Button here">Search</a>
-          </div>
+          {/* onClick sends this component's state back to the parent via searchSpotify */}
+          <button className="searchbar button" onClick={this.handleSearch}>
+            Search
+          </button>
         </div>
       </div>
     );
