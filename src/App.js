8e1274a9b9de6214af1d4b4b6dabfeee94e2e31a
Luke Patton <lpatton@dosomething.org>
2018-09-20 17:41:45 -0400

    Made a real mess trying to redirect to the Spotify Auth Service
    Working to get individual user tokens from the Spotify Authorization Service so we can search and save playlists on a user's behalf.

--- src/App.js
+++ src/App.js
@@ -21,11 +21,12 @@ class App extends Component {
   searchSpotify(term) {
     // References the Spotify utility to make a request
     // using the terms fed by the child object that gets passed this prop
-    console.log('Searching for ' + term);
-    Spotify.search(term).then(tracks => {
-      console.log('Setting the state to the search result');
-      this.setState({ results: tracks });
-    });
+    Spotify.getAuthToken();
+
+    // Spotify.search(term).then(tracks => {
+    //   console.log('Setting the state to the search result');
+    //   this.setState({ results: tracks });
+    // });
   }
 
   render() {
--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,21 +1,35 @@
-const token =
-  'BQDDlmmRk_FGzQo4A-9OhiQQFF2bMmIyteYiBuFsvGhxf96-M9PCkuFu0INMg9rzTUS_9o41F23tRmSy20vMH0j4Ks1JqCtv988jspzLD0txA2CUbCrTyG6M7eueWt_tLJ1EPUc7xyWZ6eK6ja0RMfAsZy6BeobOziav8eB-GFaWc3BDBbet40E7f7451CFJkgWjsepH_TLYw2HFiPKrtUReLsYYoGLWCsp-nSTNOs_QcAng5ynu';
+const token = '';
+const clientID = '3a721c39583b4ea89334c18ae7ea0ec1';
 
 const Spotify = {
   // Need some method for individual user auth.
+  getAuthToken() {
+    const authURL =
+      'https://cors-anywhere.herokuapp.com/https://accounts.spotify.com/authorize';
+    const queryParams =
+      '?response_type=token&&scope=user-read-private%20user-read-email&client_id=';
+    const redirectURL = '&redirect_uri=http://localhost:3000/';
+
+    console.log('Query: ' + authURL + queryParams + clientID + redirectURL);
+
+    window.location.assign(authURL + queryParams + clientID + redirectURL);
+  },
 
   search(term) {
-    const searchURL = 'https://api.spotify.com/v1/search';
+    const searchURL =
+      'https://cors-anywhere.herokuapp.com/https://api.spotify.com/v1/search';
     const queryParams = '?q=';
     const typeParams = '&type=track';
 
     console.log('Query: ' + searchURL + queryParams + term + typeParams);
     // Make a GET request to the Spotify API and search for the term
     return fetch(searchURL + queryParams + term + typeParams, {
-      // We need to send a valid access token from the Spotify Account service
       headers: { Authorization: `Bearer ${token}` },
     })
       .then(response => {
+        console.log(response);
+        // TODO: If there's no user token, go retrieve one.
+        // TODO: Clean up responses and error messages.
         if (response.ok) {
           console.log('Request ok');
           return response.json();
