b9a600510206aaeea09062c779864024633f95d0
Luke <lkpttn@gmail.com>
2018-09-21 00:09:09 -0400

    Updating results display

--- src/components/Result/Result.js
+++ src/components/Result/Result.js
@@ -2,16 +2,16 @@ import React from 'react';
 import './Result.css';
 
 class Result extends React.Component {
-  render() {
-    return (
-      <li className="result">
-        <div className="song-name">{this.props.trackName}</div>
-        <div className="artist-name">{this.props.artistName}</div>
-        <div className="album-name">{this.props.albumName}</div>
-        <div className="button">+</div>
-      </li>
-    );
-  }
+    render() {
+        return (
+            <li className="result">
+                <div className="song-name">{this.props.trackName}</div>
+                <div className="button">+</div>
+                <div className="artist-name">{this.props.artistName}</div>
+                <div className="album-name">{this.props.albumName}</div>
+            </li>
+        );
+    }
 }
 
 export default Result;
