6ceb8f7a8a221ffe34f69c432978c36aeda5aadd
Luke <lkpttn@gmail.com>
2018-09-20 23:56:50 -0400

    Swaps back to searching Spotify

--- src/App.js
+++ src/App.js
@@ -7,42 +7,44 @@ import PlaylistList from './components/PlaylistList/PlaylistList';
 import Spotify from './util/Spotify';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      results: [],
-      playlist: [],
-    };
+        this.state = {
+            results: [],
+            playlist: [],
+        };
 
-    this.searchSpotify = this.searchSpotify.bind(this);
-  }
+        this.searchSpotify = this.searchSpotify.bind(this);
+    }
 
-  searchSpotify(term) {
-    // References the Spotify utility to make a request
-    // using the terms fed by the child object that gets passed this prop
-    Spotify.getAuthToken();
+    searchSpotify(term) {
+        // Redirects to the auth url and gets an auth token
+        //  Spotify.getAuthToken();
 
-    // Spotify.search(term).then(tracks => {
-    //   console.log('Setting the state to the search result');
-    //   this.setState({ results: tracks });
-    // });
-  }
+        Spotify.search(term).then(tracks => {
+            console.log('Setting the state to the search result');
+            this.setState({ results: tracks });
+        });
+    }
 
-  render() {
-    return (
-      <div className="App">
-        <Searchbar searchSpotify={this.searchSpotify} />
-        <div className="search-results">
-          <div className="column">
-            <h2>Results</h2>
-            <List results={this.state.results} type="results" />
-          </div>
-          <PlaylistList className="column" results={this.state.playlist} />
-        </div>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <Searchbar searchSpotify={this.searchSpotify} />
+                <div className="search-results">
+                    <div className="column">
+                        <h2>Results</h2>
+                        <List results={this.state.results} type="results" />
+                    </div>
+                    <PlaylistList
+                        className="column"
+                        results={this.state.playlist}
+                    />
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,57 +1,59 @@
-const token = '';
+const token =
+    'BQArw6HvZZ5ap2m9FirhVplw9AYd-HBonLIyjWSg-mvvznv9T9S_ve-fhmG7lMynKdCRyWwkJcl9tY0relUtBWCBEQhBdQcDzKXYoXASDOCrplrCl9BvUdNTbNJYh5COujjtVcfQvPrlQ159ZSvg3TTIUhdiY13VZhM';
 const clientID = '3a721c39583b4ea89334c18ae7ea0ec1';
 
 const Spotify = {
-  // Need some method for individual user auth.
-  getAuthToken() {
-    const authURL = 'https://accounts.spotify.com/authorize';
-    const queryParams =
-      '?response_type=token&&scope=user-read-private%20user-read-email&client_id=';
-    const redirectURL = '&redirect_uri=http://localhost:3000/';
+    getAuthToken() {
+        const authURL = 'https://accounts.spotify.com/authorize';
+        const queryParams =
+            '?response_type=token&&scope=user-read-private%20user-read-email&client_id=';
+        const redirectURL = '&redirect_uri=http://localhost:3000/';
 
-    console.log('Query: ' + authURL + queryParams + clientID + redirectURL);
+        console.log('Query: ' + authURL + queryParams + clientID + redirectURL);
 
-    window.location.assign(authURL + queryParams + clientID + redirectURL);
-  },
+        window.location.assign(authURL + queryParams + clientID + redirectURL);
 
-  search(term) {
-    const searchURL =
-      'https://cors-anywhere.herokuapp.com/https://api.spotify.com/v1/search';
-    const queryParams = '?q=';
-    const typeParams = '&type=track';
+        // TODO: Pull the token out of the url and assign it to the token variable.
+    },
 
-    console.log('Query: ' + searchURL + queryParams + term + typeParams);
-    // Make a GET request to the Spotify API and search for the term
-    return fetch(searchURL + queryParams + term + typeParams, {
-      headers: { Authorization: `Bearer ${token}` },
-    })
-      .then(response => {
-        console.log(response);
-        // TODO: If there's no user token, go retrieve one.
-        // TODO: Clean up responses and error messages.
-        if (response.ok) {
-          console.log('Request ok');
-          return response.json();
-        }
-        throw new Error('Request failed!');
-      })
-      .then(jsonResponse => {
-        // Manipulate the json response here and map to an array.
-        if (jsonResponse.tracks) {
-          console.log(jsonResponse);
-          return jsonResponse.tracks.items.map(track => ({
-            id: track.id,
-            trackName: track.name,
-            artistName: track.artists[0].name,
-            albumName: track.album.name,
-          }));
-        }
-      });
-  },
+    search(term) {
+        const searchURL =
+            'https://cors-anywhere.herokuapp.com/https://api.spotify.com/v1/search';
+        const queryParams = '?q=';
+        const typeParams = '&type=track';
 
-  savePlaylist(playlist) {
-    // Make a POST request to the Spotify API and save the playlist
-  },
+        console.log('Query: ' + searchURL + queryParams + term + typeParams);
+        // Make a GET request to the Spotify API and search for the term
+        return fetch(searchURL + queryParams + term + typeParams, {
+            headers: { Authorization: `Bearer ${token}` },
+        })
+            .then(response => {
+                console.log(response);
+                // TODO: If there's no user token, go retrieve one.
+                // TODO: Clean up responses and error messages.
+                if (response.ok) {
+                    console.log('Request ok');
+                    return response.json();
+                }
+                throw new Error('Request failed!');
+            })
+            .then(jsonResponse => {
+                // Manipulate the json response here and map to an array.
+                if (jsonResponse.tracks) {
+                    console.log(jsonResponse);
+                    return jsonResponse.tracks.items.map(track => ({
+                        id: track.id,
+                        trackName: track.name,
+                        artistName: track.artists[0].name,
+                        albumName: track.album.name,
+                    }));
+                }
+            });
+    },
+
+    savePlaylist(playlist) {
+        // Make a POST request to the Spotify API and save the playlist
+    },
 };
 
 export default Spotify;
